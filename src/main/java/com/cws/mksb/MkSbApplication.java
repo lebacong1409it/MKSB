eed0e9c0fb999e8bbe8ff2847d9454da78d96cd4
Lê Bá Công <lebacong1409it@gmail.com>
2018-05-07 10:21:37 +0700

    First commit

---
+++ src/main/java/com/cws/mksb/MkSbApplication.java
@@ -0,0 +1,12 @@
+package com.cws.mksb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MkSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MkSbApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cws/mksb/MkSbApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cws.mksb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MkSbApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
